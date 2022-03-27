6dff0e0a88d81e72574af81373027208bf2ce43b
by275 <by275@protonmail.com>
2022-03-27 16:51:05 +0900

    cleanup
    * remove irrelevant files
    * apply gofmt

--- README.md
+++ README.md
@@ -3,6 +3,21 @@ go-cron
 
 Simple golang wrapper over `github.com/robfig/cron` and `os/exec` as a cron replacement
 
-## usage
+## Usage
 
-`go-cron "* * * * * *" /bin/bash -c "echo 1"`
+```bash
+$ go-cron "* * * * * *" /bin/bash -c "echo 1"
+new cron: * * * * * *
+executing: /bin/bash -c echo 1
+1
+executing: /bin/bash -c echo 1
+1
+executing: /bin/bash -c echo 1
+1
+executing: /bin/bash -c echo 1
+1
+^C(0x4ed9b0,0x56ebf0)
+Stopping
+Waiting
+Exiting
+```
--- go-cron.go
+++ go-cron.go
@@ -1,75 +1,77 @@
 package main
-import "os"
-import "os/exec"
-import "strings"
-import "sync"
-import "os/signal"
-import "syscall"
-import "github.com/robfig/cron/v3"
 
-func execute(command string, args []string)() {
+import (
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
 
-    println("executing:", command, strings.Join(args, " "))
+	"github.com/robfig/cron/v3"
+)
 
-    cmd := exec.Command(command, args...)
+func execute(command string, args []string) {
 
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
+	println("executing:", command, strings.Join(args, " "))
 
-    cmd.Run()
+	cmd := exec.Command(command, args...)
 
-    cmd.Wait()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
+
+	cmd.Wait()
 }
 
 func create() (cr *cron.Cron, wgr *sync.WaitGroup) {
-    var schedule string = os.Args[1]
-    var command string = os.Args[2]
-    var args []string = os.Args[3:len(os.Args)]
+	var schedule string = os.Args[1]
+	var command string = os.Args[2]
+	var args []string = os.Args[3:len(os.Args)]
 
-    wg := &sync.WaitGroup{}
+	wg := &sync.WaitGroup{}
 
-    c := cron.New(
-      cron.WithParser(
-        cron.NewParser(
-          cron.SecondOptional | cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow | cron.Descriptor,
-        ),
-      ),
-    )
-    println("new cron:", schedule)
+	c := cron.New(
+		cron.WithParser(
+			cron.NewParser(
+				cron.SecondOptional | cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow | cron.Descriptor,
+			),
+		),
+	)
+	println("new cron:", schedule)
 
-    c.AddFunc(schedule, func() {
-        wg.Add(1)
-        execute(command, args)
-        wg.Done()
-    })
+	c.AddFunc(schedule, func() {
+		wg.Add(1)
+		execute(command, args)
+		wg.Done()
+	})
 
-    return c, wg
+	return c, wg
 }
 
 func start(c *cron.Cron, wg *sync.WaitGroup) {
-    c.Start()
+	c.Start()
 }
 
 func stop(c *cron.Cron, wg *sync.WaitGroup) {
-    println("Stopping")
-    c.Stop()
-    println("Waiting")
-    wg.Wait()
-    println("Exiting")
-    os.Exit(0)
+	println("Stopping")
+	c.Stop()
+	println("Waiting")
+	wg.Wait()
+	println("Exiting")
+	os.Exit(0)
 }
 
 func main() {
 
-    c, wg := create()
+	c, wg := create()
 
-    go start(c, wg)
+	go start(c, wg)
 
-    ch := make(chan os.Signal, 1)
-    signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-    println(<-ch)
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	println(<-ch)
 
-    stop(c, wg)
+	stop(c, wg)
 }
-
-
